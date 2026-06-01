5353ebb0cbb07c7883c371b392b50ec836dd967f
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-06-01 16:18:29 +0800

    feat(reproductions): add fedavg and fedprox

---
+++ reproductions/fedavg_mnist/tests/data_test.py
@@ -0,0 +1,24 @@
+"""Used to test the model and the data partitionning."""
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+import dataset
+
+
+def test_non_iid_partitionning(num_clients: int = 100) -> None:
+    """Test the non iid partitionning of the MNIST dataset.
+
+    Parameters
+    ----------
+    num_clients : int, optional
+        The number of clients to distribute the data to, by default 100
+    """
+    trainloaders, _, _ = dataset.load_datasets(
+        batch_size=1, num_clients=num_clients, iid=False, balance=True
+    )
+    for trainloader in trainloaders:
+        labels = []
+        for _, label in trainloader:
+            labels.append(label.item())
+        assert len(set(labels)) <= 2
